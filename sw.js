2fc80e94304d4a7b8709b584ddf6431be31e6b5e
d7representacao-hub <d7representacao@gmail.com>
2026-06-10 06:02:55 -0300

    chore: bump SW para d7hub-v13

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'd7hub-v12';
+const CACHE_NAME = 'd7hub-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-152.png'];
 
 self.addEventListener('install', e => {
