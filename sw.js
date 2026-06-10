182e12dda8586114f993ae5971b8f5d1789c0789
d7representacao-hub <d7representacao@gmail.com>
2026-06-10 16:50:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'd7hub-v78';
+const CACHE_NAME = 'd7hub-v79';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-152.png'];
 
 self.addEventListener('install', e => {
